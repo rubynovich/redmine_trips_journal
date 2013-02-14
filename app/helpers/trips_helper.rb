5097f60dabb6d5aa8a88dd2ab08b1e1704d5fcf1
rubynovich <roman@shipiev.me>
2013-02-14 14:23:04 +0400

    fix

--- app/helpers/trips_helper.rb
+++ app/helpers/trips_helper.rb
@@ -1,6 +1,6 @@
 module TripsHelper
   def label_tag_for(field, options = {})
-    label_text = l(("field_"+field.to_s.gsub(/\_id$/, "")).to_sym) + (options.delete(:required) ? content_tag("span", " *", :class => "required"): "")
+    label_text = l(("field_"+field.to_s.gsub(/\_id$/, "")).to_sym) + (options.delete(:required) ? content_tag("span", " *", :class => "required"): "").html_safe
     content_tag("label", label_text)
   end
 end
